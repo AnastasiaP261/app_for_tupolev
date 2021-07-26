fcf48e1c57ba562d1128bcf84e184be0460c94b9
APetrichuk <petrichuk.nastya@yandex.ru>
2021-07-26 13:00:59 +0300

    добалена генерация предзаполненной формы из таблицы ексель

--- Users/views.py
+++ Users/views.py
@@ -5,6 +5,7 @@ from Users.models import *
 from Licenseservers.models import *
 from .forms import UploadExcelFileForm
 import openpyxl
+from random import randint
 from django.core.files.storage import FileSystemStorage
 
 
@@ -13,6 +14,20 @@ class ViewAddingOrders(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        context['empty'] = self.request.session.get('empty')
+        context['table_data'] = self.request.session.get('table_data')
+        try:
+            del self.request.session['empty']
+            del self.request.session['table_data']
+        except:
+            pass
+
+        if context['empty'] == None:
+            context['empty'] = True
+
+        print(context['empty'])
+        print(context['table_data'])
 
         context['sites'] = Licenseservers.objects.values_list('site', flat=True).distinct()
         context['lic_servers'] = Licenseservers.objects.values_list('name', flat=True).distinct()
@@ -73,13 +88,10 @@ class ViewGroupmembers(ListView):
 def upload_excel(request):
     if request.method == 'POST' and request.FILES['file']:
         file = request.FILES['file']
-        handle_uploaded_file(file)
-        fs = FileSystemStorage()
-        filename = fs.save(file.name, file)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'core/simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+        table_data = handle_uploaded_file(file)
+        request.session['empty'] = False
+        request.session['table_data'] = table_data
+        return redirect('adding_orders')
     # if request.method == 'POST':
     #     form = UploadExcelFileForm(request.POST, request.FILES)
     #     if form.is_valid():
@@ -98,18 +110,44 @@ def handle_uploaded_file(f):
 
     req_num = sheet['A2'].value
     for row in range(2, sheet.max_row + 1):
+        trans_name = transliterate(sheet[(f"B{row}")].value.split(' ')[1][:1]) \
+                     + transliterate(sheet[(f"B{row}")].value.split(' ')[0])
+        trans_name = trans_name.lower()
+        print(trans_name)
         table_data.append({
             'req_num': f'{req_num}',
             'full_name': f'{sheet[(f"B{row}")].value}',
             'os_name': f'{sheet[(f"F{row}")].value}',
-            'tc_name': f'',
-            'tc_pass': f'',
+            'tc_name': f'{trans_name}',
+            'tc_pass': f'{trans_name + str(randint(111, 999))}',
             'group': f'{sheet[(f"D{row}")].value}',
             'role': f'{sheet[(f"E{row}")].value}',
             'lic_server': f'{sheet[(f"G{row}")].value}',
             'site': f'{sheet[(f"H{row}")].value}',
-            })
+        })
 
-    return redirect('adding_orders', kwargs={'table': table_data})
+    return table_data
 
 
+def transliterate(name):
+    # Слоаврь с заменами
+    slovar = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+        'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
+        'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e',
+        'ю': 'yu', 'я': 'ya',
+
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo',
+        'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N',
+        'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'H',
+        'Ц': 'C', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E',
+        'Ю': 'Yu', 'Я': 'Ya',
+
+        ' ': ' ',
+    }
+
+    # Циклически заменяем все буквы в строке
+    for key in slovar:
+        name = name.replace(key, slovar[key])
+    return name
